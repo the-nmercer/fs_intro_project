09bcc46e3c4a0aaccec84335b9e733fe50506723
the-nmercer <thenicholasmercer@gmail.com>
2024-10-27 19:52:28 -0500

    Added simple search function.

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,6 +1,12 @@
 class DogsController < ApplicationController
   def index
-    @dogs = Dogs.page(params[:page]).per(20)
+    if params[:search].present?
+      @dogs = Dog.joins(:owner)
+                 .where('LOWER(dogs.breed) LIKE LOWER(?) OR LOWER(owners.name) LIKE LOWER(?)', "%#{params[:search]}%", "%#{params[:search]}%")
+                 .page(params[:page]).per(20)
+    else
+      @dogs = Dog.all.page(params[:page]).per(20)
+    end
   end
 
   def show
